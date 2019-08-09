423dfe01f42fe907d4006edcb53322cba05e1d84
Bernard Cooley <>
2019-08-09 10:17:13 +0100

    second commit

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import ExploreScreen from '../screens/ExploreScreen';
+import UploadScreen from '../screens/UploadScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const HomeStack = createStackNavigator(
+  {
+    Explore: ExploreScreen,
+  },
+  config
+);
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Explore',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+HomeStack.path = '';
+
+const LinksStack = createStackNavigator(
+  {
+    Upload: UploadScreen,
+  },
+  config
+);
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Upload',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
+  ),
+};
+
+LinksStack.path = '';
+
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+  },
+  config
+);
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+SettingsStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
